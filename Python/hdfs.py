db56ad0c0d9f31f1fd6d0b7b312a30e64a5df39c
patatefroide <mercierthomas2022@gmail.com>
2023-09-13 14:43:41 +0200

    debut

---
+++ Python/hdfs.py
@@ -0,0 +1,8 @@
+from hdfs import InsecureClient
+
+hadoop_address = 'http://172.22.0.6:9870/'
+client = InsecureClient(hadoop_address, user='root')
+file = 'texte.txt'
+client.makedirs('/projet')
+hdfs_path = '/projet/' + file
+client.upload(hdfs_path, file)
