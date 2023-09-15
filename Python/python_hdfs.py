951f9a45c64970d611257f2088287a6d4f43d4ae
patatefroide <mercierthomas2022@gmail.com>
2023-09-15 11:17:39 +0200

    spark

--- Python/python_hdfs.py
+++ Python/python_hdfs.py
@@ -1,5 +1,7 @@
 from hdfs import InsecureClient
-#from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession
+import pyspark
+from delta import *
 
 hadoop_address = 'http://172.24.0.6:9870/'
 client = InsecureClient(hadoop_address, user='root')
@@ -16,3 +18,26 @@ with open(fichier, 'r') as f:
     read = f.read()
     print(read)
 
+  
+spark = SparkSession.builder.getOrCreate()
+df = spark.read.csv(fichier)
+df.selectExpr("split(_c0, ' ') as texte").show(4,False)
+
+
+
+builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
+
+# créer une table
+data = spark.range(0, 5)
+data.write.format("delta").save("/tmp/delta-table")
+
+# lire 
+df = spark.read.format("delta").load("/tmp/delta-table")
+df.show()
+
+# écrire
+data = spark.range(5, 10)
+data.write.format("delta").mode("overwrite").save("/tmp/delta-table")
