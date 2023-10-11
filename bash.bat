451ddccf50c36644400a963f08e73a1100068478
Gaetan4 <gaetanallahasra@yahoo.com>
2023-10-12 00:14:14 +0200

    bash

--- bash.bat
+++ bash.bat
@@ -10,6 +10,9 @@ cd ..
 docker cp namenode:/etc/hadoop/core-site.xml ./core-site.xml
 docker cp ./core-site.xml nifi:/opt/nifi/nifi-current/conf/core-site.xml
 docker cp NIFIFLOW.xml nifi:/opt/nifi/nifi-current/conf/NIFIFLOW.xml
+docker cp NIFI.html nifi:/opt/nifi/nifi-current/conf/NIFI.html
+
+timeout /t 30 >nul
 
 docker-compose build
 docker-compose up
