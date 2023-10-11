cc43cd9fba48324d36fa34c567f9cd833c32dbc5
Gaetan4 <gaetanallahasra@yahoo.com>
2023-10-12 01:38:09 +0200

    bash

--- bash.bat
+++ bash.bat
@@ -3,7 +3,7 @@ docker network create projet_big_data_network
 cd Docker_services
 docker-compose up -d
 
-timeout /t 30 >nul
+timeout /t 10 >nul
 
 cd ..
 
@@ -11,9 +11,5 @@ docker cp namenode:/etc/hadoop/core-site.xml ./core-site.xml
 docker cp ./core-site.xml nifi:/opt/nifi/nifi-current/conf/core-site.xml
 docker cp NIFIFLOW.xml nifi:/opt/nifi/nifi-current/conf/NIFIFLOW.xml
 docker cp NIFI.html nifi:/opt/nifi/nifi-current/conf/NIFI.html
-
-timeout /t 30 >nul
-
-docker-compose build
-docker-compose up
+docker cp NIFIFLOW.xml nifi:/opt/nifi/nifi-current/conf/archive/NIFIFLOW.xml
 
