817dfff0bc8e696637ff4bad4ea79bfc47aa4ff4
ThomasMerci <mercierthomas2022@gmail.com>
2023-10-11 07:04:58 +0200

    delta

--- Python/python_deltalake.py
+++ Python/python_deltalake.py
@@ -8,11 +8,11 @@ from pyspark.sql import SparkSession
 from delta import *
 import pyarrow.hdfs as hdfs
 import pandas as pd
+from delta import DeltaTable
 import python_extract
 #import python_ml
 #import data_analysis
 #import data_processing
-
 
 #mise en place des fihciers dans hdfs 
 hadoop_address = 'http://namenode:9870/' #namenode
@@ -20,68 +20,47 @@ client = InsecureClient(hadoop_address, user='root')
 client.makedirs('/projet')
 print(hadoop_address)
 
-#orders
-orders = './orders.csv'
-if os.path.exists(orders):
-    print(f"Le fichier {orders} existe.")
-else:
-    print(f"Le fichier {orders} n'existe pas")
-    exit(1)
-#vers hdfs
-fichier_hdfs = '/projet/' + orders.split('/')[-1]
-client.upload(fichier_hdfs,orders, overwrite=True)
+#hdfs enregistrement
+def upload_hdfs(local, hdfs, client):
+    try:
+        if not os.path.exists(local):
+            print(f"Le fichier {local} n'existe pas localement. Téléchargement depuis HDFS...")
+            fichier_hdfs = hdfs + local.split('/')[-1]
+            client.upload(fichier_hdfs, local, overwrite=True)
+        else:
+            print(f"Le fichier {local} existe localement. Utilisation du fichier local.")
 
-#bikes
-bikes = './bikes.csv'
-if os.path.exists(bikes):
-    print(f"Le fichier {bikes} existe.")
-else:
-    print(f"Le fichier {bikes} n'existe pas")
-    exit(1)
-#vers hdfs
-fichier_hdfs = '/projet/' + bikes.split('/')[-1]
-client.upload(fichier_hdfs,bikes, overwrite=True)
+    except Exception as e:
+        print(f"Erreur lors du traitement de {local}: {e}")
 
-#bikeshops
-bikeshops = './bikeshops.csv'
-if os.path.exists(bikeshops):
-    print(f"Le fichier {bikeshops} existe.")
-else:
-    print(f"Le fichier {bikeshops} n'existe pas")
-    exit(1)
-#vers hdfs
-fichier_hdfs = '/projet/' + bikeshops.split('/')[-1]
-client.upload(fichier_hdfs,bikeshops, overwrite=True)
+upload_hdfs('./orders.csv', '/projet/', client)
+upload_hdfs('./bikes.csv', '/projet/', client)
+upload_hdfs('./bikeshops.csv', '/projet/', client)
 
+#hdfs lecture
+dfs = {}
+hdfs_csvs = ['/projet/orders.csv', '/projet/bikes.csv', '/projet/bikeshops.csv']
+for hdfs_csv in hdfs_csvs:
+    local = './' + hdfs_csv.split('/')[-1]
+    if not os.path.exists(local):
+        client.download(hdfs_csv, local)
+        print(f"Téléchargement de {local} depuis HDFS.")
+    else:
+        print(f"Le fichier {local} existe localement. Utilisation du fichier local.")
 
-#hdfs
-hadoop_address = 'http://namenode:9870/'
-client = InsecureClient(hadoop_address, user='root')
+    with client.read(hdfs_csv, encoding='utf-8') as hdfs_data:
+        dfs[local] = pd.read_csv(hdfs_data)
 
-#csv
-df_orders_csv = '/projet/orders.csv'
-df_bikes_csv = '/projet/bikes.csv'
-df_bikeshops_csv = '/projet/bikeshops.csv'
-#client.download(df_orders_csv, './')
+for key in dfs.keys():
+    print(f"fichier : {key}")
 
-with client.read(df_orders_csv, encoding='utf-8') as hdfs_file:
-    df_orders_csv = pd.read_csv(hdfs_file)
-with client.read(df_bikes_csv, encoding='utf-8') as hdfs_file:
-    df_bikes_csv = pd.read_csv(hdfs_file)
-with client.read(df_bikeshops_csv, encoding='utf-8') as hdfs_file:
-    df_bikeshops_csv = pd.read_csv(hdfs_file)
+df_ml, data_bikes = python_extract.extract('./orders.csv', './bikes.csv', './bikeshops.csv')
+df_ml.show(10)
 
 #script python
 #DataAnalysis.analyze_and_clean_data()
 #data_processing.processing_and_save_data(df_orders_csv, df_bikes_csv, df_bikeshops_csv)
 
-#join, data pour le ML
-df_ml, data_bikes = python_extract.extract(df_orders_csv, df_bikes_csv, df_bikeshops_csv)
-print("lecture")
-#df_ml.show()
-#data_bikes.show()
-
-"""
 #spark
 builder = pyspark.sql.SparkSession.builder.appName("deltalake") \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
@@ -107,19 +86,33 @@ csv_data = csv_data.na.drop()
 csv_data.show(10)
 
 
-# delta lecture
+# delta 
 csv_data.write.format("delta").mode("overwrite").save("/tmp/csv_table")
 df_csv = spark.read.format("delta").load("/tmp/csv_table")
 df_csv.show(10)
-"""
+data_bikes.show(10)
 
-# hdfs lecture
-fichier_hdfs = '/projet/bikes.csv'
-with client.read(fichier_hdfs, encoding='utf-8') as hdfs_file:
-    df = pd.read_csv(hdfs_file)
-    print(df.head())
-    print(df.describe())
+#enregistrement df_ml dans deltalake
+df_ml.write.format("delta").mode("overwrite").save("/tmp/df_ml")
+data_bikes.write.format("delta").mode("overwrite").save("/tmp/data_bikes")
+df_ml_delta = spark.read.format("delta").load("/tmp/df_ml")
+data_bikes_delta = spark.read.format("delta").load("/tmp/data_bikes")
+data_bikes_delta.show(10)
+df_ml.show(10)
+
+#lister les dossiers delta
+import os
+listes = os.listdir("/tmp/")
+for file in listes:
+    chemin = os.path.join("/tmp/", file)
+    if os.path.isfile(chemin):
+        print(f"Fichier : {file}")
+    elif os.path.isdir(chemin):
+        print(f"Dossier : {file}")
 
 
 
 print('fin')
+
+for local_csv in dfs:
+    os.remove(local_csv)
--- Python/python_extract.py
+++ Python/python_extract.py
@@ -1,21 +1,53 @@
-from pyspark.sql.functions import dayofmonth, dayofweek, to_date, datediff, lit, date_add, coalesce, col
+from pyspark.sql.functions import dayofmonth, dayofweek, to_date, datediff, lit, date_add, coalesce, col, StructType
 from pyspark.sql.types import DoubleType, FloatType, IntegerType
 from pyspark.sql import DataFrame, functions as Funct
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 import pyspark
 from pyspark.sql import SparkSession
 
 def extract(df_orders, df_bikes, df_bikeshops):
 
     spark = SparkSession.builder.appName("data").enableHiveSupport().getOrCreate()
-    df_orders = spark.createDataFrame(df_orders)
-    df_bikes = spark.createDataFrame(df_bikes)
-    df_bikeshops = spark.createDataFrame(df_bikeshops)
 
-    
-    df_orders = df_orders.withColumnRenamed("product.id", "bike")
-    df_orders = df_orders.withColumnRenamed("order.id", "order_id")
-    df_orders = df_orders.withColumnRenamed("order.date", "order_date")
-    df_orders = df_orders.withColumnRenamed("customer.id", "bikeshop")
+    ordersSchema = StructType([
+            StructField("id", IntegerType(), True),
+            StructField("order_id", IntegerType(), True),
+            StructField("order_line", IntegerType(), True),
+            StructField("order_date", StringType(), True),
+            StructField("customer_id", IntegerType(), True),
+            StructField("product_id", IntegerType(), True),
+            StructField("quantity", IntegerType(), True)])
+    df_orders = (spark.read.format("csv").option("inferSchema", "false").option("delimiter", ";").option("header", "true").schema(ordersSchema).load(df_orders))
+    df_orders = df_orders.withColumn("order_date", to_date(df_orders["order_date"], "M/d/yyyy"))
+
+    bikesSchema = StructType([
+            StructField("bike_id", IntegerType(), True),
+            StructField("model", StringType(), True),
+            StructField("category1", StringType(), True),
+            StructField("category2", StringType(), True),
+            StructField("frame", StringType(), True),
+            StructField("price", DoubleType(), True)])
+    df_bikes = (spark.read.format("csv").option("inferSchema", "false").option("delimiter", ";").option("header", "true").schema(bikesSchema).load(df_bikes))
+
+    bikeshopSchema = StructType([
+            StructField("bikeshop_id", IntegerType(), True),
+            StructField("bikeshop_name", StringType(), True),
+            StructField("bikeshop_city", StringType(), True),
+            StructField("bikeshop_state", StringType(), True),
+            StructField("latitude", StringType(), True),
+            StructField("longitude", StringType(), True)])
+    df_bikeshops = (spark.read.format("csv").option("inferSchema", "false").option("delimiter", ";").option("header", "true").schema(bikeshopSchema).load(df_bikeshops))
+
+    #df_orders = spark.createDataFrame(df_orders)
+    #df_bikes = spark.createDataFrame(df_bikes)
+    #df_bikeshops = spark.createDataFrame(df_bikeshops)
+
+
+    df_orders = df_orders.withColumnRenamed("product_id", "bike")
+    df_orders = df_orders.withColumnRenamed("order_id", "order_id")
+    df_orders = df_orders.withColumnRenamed("order_date", "order_date")
+    df_orders = df_orders.withColumnRenamed("customer_id", "bikeshop")
 
     df_orders = df_orders.withColumn("order_date", to_date(df_orders["order_date"], "M/d/yyyy"))
     df_orders = df_orders.withColumn("jour", dayofmonth(df_orders["order_date"]))
@@ -26,10 +58,10 @@ def extract(df_orders, df_bikes, df_bikeshops):
     df_orders = df_orders.fillna(0, subset=["jour"])
 
     #bikes.csv
-    df_bikes = df_bikes.withColumnRenamed("bike.id", "bike")
+    df_bikes = df_bikes.withColumnRenamed("bike_id", "bike")
 
     #bikeshop.csv
-    df_bikeshops = df_bikeshops.withColumnRenamed("bikeshop.id", "bikeshop")
+    df_bikeshops = df_bikeshops.withColumnRenamed("bikeshop_id", "bikeshop")
 
     #join
     df_1 = df_orders.join(df_bikes, on="bike", how="inner")
@@ -49,6 +81,7 @@ def extract(df_orders, df_bikes, df_bikeshops):
     df_ML = df_ML.withColumn("nb_jour", datediff(df_ML["order_date"], lit(min_date)))
     df_ML = df_ML.fillna(0, subset=["jour"])
 
+    df.show
     return df_ML, df
 
 
