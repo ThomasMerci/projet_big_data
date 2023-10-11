f1037b6055b766744328c2356ec01e9c894dcb86
Gabriello ZAFIFOMENDRAHA <gabriello.zafifomendraha@gmail.com>
2023-10-11 12:47:54 +0200

    Ajout data nettoyage et analyse dans python_deltalake.py

--- Python/python_deltalake.py
+++ Python/python_deltalake.py
@@ -1,12 +1,12 @@
 import pyspark
 from delta import *
+from pyspark.sql.functions import *
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType
 from hdfs import InsecureClient
 import os
 from hdfs import InsecureClient
 from pyspark.sql import SparkSession
 from delta import *
-import pyarrow.hdfs as hdfs
 import pandas as pd
 from delta import DeltaTable
 import python_extract
@@ -14,7 +14,7 @@ import python_ml
 #import data_analysis
 #import data_processing
 
-#mise en place des fihciers dans hdfs 
+#mise en place des fihciers dans hdfs
 hadoop_address = 'http://namenode:9870/' #namenode
 client = InsecureClient(hadoop_address, user='root')
 client.makedirs('/projet')
@@ -52,6 +52,7 @@ for hdfs_csv in hdfs_csvs:
         dfs[local] = pd.read_csv(hdfs_data)
 
 df_ml, data_bikes = python_extract.extract('./orders.csv', './bikes.csv', './bikeshops.csv')
+data_bikes.show(10)
 df_ml.show(10)
 
 for key in dfs.keys():
@@ -73,18 +74,18 @@ df = spark.read.format("delta").load("/projet/data_table")
 df.show()
 
 # csv
-schema = StructType([StructField("id", IntegerType(), True), 
-    StructField("model", StringType(), True),
-    StructField("category1", StringType(), True),
-    StructField("category2", StringType(), True),
-    StructField("frame", StringType(), True),
-    StructField("price", DoubleType(), True) ])
+schema = StructType([StructField("id", IntegerType(), True),
+                     StructField("model", StringType(), True),
+                     StructField("category1", StringType(), True),
+                     StructField("category2", StringType(), True),
+                     StructField("frame", StringType(), True),
+                     StructField("price", DoubleType(), True) ])
 data_csv = "./texte.csv"
 csv_data = spark.read.option("header", "true").option("delimiter", ";").schema(schema).csv(data_csv)
 csv_data = csv_data.na.drop()
 csv_data.show(10)
 
-# delta 
+# delta
 csv_data.write.format("delta").mode("overwrite").save("/projet/csv_table")
 df_csv = spark.read.format("delta").load("/projet/csv_table")
 df_csv.show(10)
@@ -118,6 +119,32 @@ for file in listes:
 
 #df_hdfs = spark.read.format("delta").load("hdfs://namenode:8020/projet/data_table")
 #df_hdfs.show()
+
+# Analyse
+print("Calcul des corrélations de Pearson pour chaque paire de colonne : \n")
+# Calculate Pearson correlation
+num_col = []
+
+# Convert string columns to double when possible
+for column in data_bikes.columns:
+    try:
+        if int(data_bikes.select(column).first()[0]):
+            num_col += [column]
+    except:
+        pass
+
+# Calculer la corrélation de pearson pour chaque pair de colonne
+correlations = {}
+for i in range(len(num_col)):
+    for j in range(i+1, len(num_col)):
+        col_i = num_col[i]
+        col_j = num_col[j]
+        corr_val = data_bikes.select(corr(col_i, col_j)).collect()[0][0]
+        correlations[(col_i, col_j)] = corr_val
+
+# Afficher le résultat
+for cols, corr_val in correlations.items():
+    print("La corrélation de Pearson entre {} et {} est : {}".format(cols[0], cols[1], corr_val))
 
 print('fin')
 
