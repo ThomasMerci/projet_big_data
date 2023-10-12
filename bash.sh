b36eab93384f01164fd9083e490c05163c8a732e
ThomasMerci <mercierthomas2022@gmail.com>
2023-10-12 14:55:34 +0200

    deltalake-hdfs

--- bash.sh
+++ bash.sh
@@ -9,7 +9,12 @@ cd ..
 
 docker cp namenode:/etc/hadoop/core-site.xml ./core-site.xml
 docker cp ./core-site.xml nifi:/opt/nifi/nifi-current/conf/core-site.xml
+docker cp NIFIFLOW.xml nifi:/opt/nifi/nifi-current/conf/NIFIFLOW.xml
+docker cp NIFI.html nifi:/opt/nifi/nifi-current/conf/NIFI.html
+docker cp NIFIFLOW.xml nifi:/opt/nifi/nifi-current/conf/archive/NIFIFLOW.xml
 
-docker-compose build
-docker-compose up
-
+docker cp Python/texte.csv nifi:/opt/nifi/nifi-current/conf/texte.csv
+docker cp data/bikes.csv nifi:/opt/nifi/nifi-current/conf/bikes.csv
+docker cp data/bikeshops.csv nifi:/opt/nifi/nifi-current/conf/bikeshops.csv
+docker cp data/orders.csv nifi:/opt/nifi/nifi-current/conf/orders.csv
+docker cp data/customers.csv nifi:/opt/nifi/nifi-current/conf/customers.csv
