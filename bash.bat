4a9d820bc50d970d67e879e45530cb0c7c3e0bf1
ThomasMerci <mercierthomas2022@gmail.com>
2023-10-12 13:31:25 +0200

    bash

--- bash.bat
+++ bash.bat
@@ -13,20 +13,8 @@ docker cp NIFIFLOW.xml nifi:/opt/nifi/nifi-current/conf/NIFIFLOW.xml
 docker cp NIFI.html nifi:/opt/nifi/nifi-current/conf/NIFI.html
 docker cp NIFIFLOW.xml nifi:/opt/nifi/nifi-current/conf/archive/NIFIFLOW.xml
 
-
-docker network create projet_big_data_network
-cd Docker_services
-docker-compose up -d
-
-timeout /t 30 >nul
-
-cd ..
-
-docker cp namenode:/etc/hadoop/core-site.xml ./core-site.xml
-docker cp ./core-site.xml nifi:/opt/nifi/nifi-current/conf/core-site.xml
-docker cp NIFIFLOW.xml nifi:/opt/nifi/nifi-current/conf/NIFIFLOW.xml
-docker cp NIFI.html nifi:/opt/nifi/nifi-current/conf/NIFI.html
-
-timeout /t 30 >nul
-
-
+docker cp Python/texte.csv nifi:/opt/nifi/nifi-current/conf/texte.csv
+docker cp data/bikes.csv nifi:/opt/nifi/nifi-current/conf/bikes.csv
+docker cp data/bikeshops.csv nifi:/opt/nifi/nifi-current/conf/bikeshops.csv
+docker cp data/orders.csv nifi:/opt/nifi/nifi-current/conf/orders.csv
+docker cp data/customers.csv nifi:/opt/nifi/nifi-current/conf/customers.csv
