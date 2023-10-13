7e868ce61134a7c76ba4d27d622499cfe4f6aca5
ThomasMerci <mercierthomas2022@gmail.com>
2023-10-13 03:43:03 +0200

    python

---
+++ bash_power.bat
@@ -0,0 +1,2 @@
+
+docker cp nifi:/opt/nifi/nifi-current/conf/data_bikes.csv data/data_bikes.csv 
--- bash_power.sh
+++ bash_power.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 docker cp nifi:/opt/nifi/nifi-current/conf/data_bikes.csv data/data_bikes.csv 
-docker cp nifi:/opt/nifi/nifi-current/conf/df_ml.csv data/df_ml.csv
